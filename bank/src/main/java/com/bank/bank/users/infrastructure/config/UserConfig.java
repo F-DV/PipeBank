37e706d8e5b51f8ecde81d9f66374677f98fc41e
Felipe <fquiceno1@misena.edu.co>
2023-04-02 22:22:56 -0500

    [Delete]: Se borra el servicio que tenia en el domain

--- bank/src/main/java/com/bank/bank/users/infrastructure/config/UserConfig.java
+++ bank/src/main/java/com/bank/bank/users/infrastructure/config/UserConfig.java
@@ -4,18 +4,11 @@ import com.bank.bank.users.applications.services.UserService;
 import com.bank.bank.users.applications.usecases.CreateUserUseCaseImpl;
 import com.bank.bank.users.applications.usecases.GetUserUseCaseImpl;
 import com.bank.bank.users.domain.ports.out.UserRepositoryPort;
-import com.bank.bank.users.domain.ports.out.UserServicePort;
-import com.bank.bank.users.domain.ports.UserServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class UserConfig {
-
-    @Bean
-    public UserServicePort userServicePort(UserRepositoryPort userRepositoryPort){
-        return new UserServiceImpl(userRepositoryPort);
-    }
 
     @Bean
     public UserService userService(UserRepositoryPort userRepositoryPort){
